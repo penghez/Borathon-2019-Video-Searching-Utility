3cb63f5fb8b8be23da8b0abd443b09cc7cc6cbc2
zpenghe <zpenghe@vmware.com>
2019-06-27 13:39:53 -0700

    modify return format

--- Flask Server/app.py
+++ Flask Server/app.py
@@ -35,8 +35,20 @@ def search():
     }
 
     resp = es.search(index="transcript", doc_type="_doc", body=body)
-    resp = jsonify(resp['hits']['hits'])
+    
+    resp_for_tf = resp['hits']['hits']
+    resp_group_by_name = {}
+    for resp_item in resp_for_tf:
+        group_items = resp_group_by_name.get(resp_item['_source']['video_name'], [])
+        resp_source = resp_item['_source']
+        resp_source['highlight'] = resp_item['highlight']['row_content'][0]
+        group_items.append(resp_source)
+        resp_group_by_name[resp_item['_source']['video_name']] = group_items
 
+    resp_values = list(resp_group_by_name.values())
+    print(resp_values)
+
+    resp = jsonify(resp['hits']['hits'])
     resp = make_response(resp, 200)
     resp.headers.add("Access-Control-Allow-Origin", "*")
     resp.headers.add("Access-Control-Allow-Headers", "*")
